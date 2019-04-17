f916ff2fd71e4c0e7150d108a8d03bf16d507b78
Alex <alexandrareginawilson@gmail.com>
2019-04-17 14:01:18 -0500

    axios

--- controller/controller.js
+++ controller/controller.js
@@ -4,12 +4,51 @@ var passport = require("../config/passport");
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 
 var router = express.Router();
+
+var axios = require("axios");
+
+
+
+
+//HTML ROUTES======================================================================================================
+router.get("/", function(req, res) {
+    //Checking if session exists for current user.
+    console.log(req.user);
+    if (req.user) {
+        return res.redirect("/members");
+    }
+
+    res.render("index", {
+        msg: "Welcome!"
+    });
+});
+
+// Load example page and pass in an example by id
+// router.get("/login", function(req, res) {
+//     //Session exists for the user
+//     console.log(req.user);
+//     if (req.user) {
+//         return res.redirect("/members");
+//     }
+
+//     //Else render the login.handlbars
+//     res.render("index");
+
+// });
+
+router.get("/members", isAuthenticated, function(req, res) {
+    console.log("reaching member page");
+    res.render("members", {user: req.user.username});
+
+});
+
+
 //API ROUTES======================================================================================================
 //logging in route
 router.post("/api/login/", passport.authenticate("local"), function(req, res) {
+    console.log("going to members");
     res.json("/members");
 });
-
 //signing up account route
 router.post("/api/signup", function(req, res) {
     console.log(req.body.username);
@@ -18,6 +57,7 @@ router.post("/api/signup", function(req, res) {
         username: req.body.username,
         password: req.body.password
     }).then(function() {
+      
         res.redirect(307, "/api/login");
     }).catch(function(err) {
         console.log("Getting error");
@@ -25,9 +65,6 @@ router.post("/api/signup", function(req, res) {
         // res.status(422).json(err.errors[0].message);
     });
 });
-
-
-
 
 // Route for logging user out
 router.get("/logout", function(req, res) {
@@ -51,97 +88,11 @@ router.get("/api/user_data", function(req, res) {
     }
 });
 
-
-
-//HTML ROUTES======================================================================================================
-router.get("/members/favorites", function(req, res) {
-    //Checking if session exists for current user.
-    
-    console.log(req.user);
-    if (!req.user) {
-        return res.redirect("/");
-    }
-    // console.log(req.user.username);
-    db.User.findAll({
-        where: {id: req.user.id},
-        include: [{
-            model: db.Meal,
-            attributes: ["name", "image", "recipeURL"],
-            through: {
-                model: db.Favorite
-            }
-        }]
-    })
-        .then(function(results){
-            //Pull data from database
-            var recipeList = results[0].Meals;
-            console.log(recipeList);
-            console.log(results[0].Meals[0].name);
-            console.log(results[0].Meals[1].name);
-            var recipes = [];
-            for (var i = 0; i < recipeList.length; i++){
-
-                var recipe = {
-                    recipeName: recipeList[i].name,
-                    imageLink: recipeList[i].image,
-                    recipeLink: recipeList[i].recipeURL
-                };
-
-                recipes.push(recipe);
-            }
-            console.log(recipes);
-            
-
-
-            res.render("favorites", {
-                user: req.user.username,
-                recipes: recipes
-            });
-
-        });
-
-
-
-});
-
-router.get("/members", isAuthenticated, function(req, res) {
-    console.log("reaching member page");
-    res.render("members", {user: req.user.username});
-
-});
-
-
-
-router.get("/", function(req, res) {
-    //Checking if session exists for current user.
-    if (req.user) {
-        return res.redirect("/members");
-    }
-
-    res.render("index", {
-        msg: "Welcome!"
-    });
-});
-
-
-
-// Render 404 page for any unmatched routes
-router.get("*", function(req, res) {
-    res.render("404");
-});
-
-
-
-
 // eslint-disable-next-line no-unused-vars
 router.post("/api/meals", function(req, res) {
 
     console.log(req.body);
-    // db.Meal.findOrCreate({
-    //     where: {
-    //         name:
-    //     }
-    // })
+
 
 });
 
@@ -149,6 +100,46 @@ router.get("/form", function(req, res) {
 
     res.render("form");
 
+});
+
+router.post("/api/recipe", function(req, res) {
+	
+    axios.get(req.body.sendingURL).then(function(response) {
+        var data=response.data;
+        var arr=[];
+        
+        for (var i = 0; i < data.hits.length; i ++){
+            var caloriesPer = parseFloat(data.hits[i].recipe.calories)/parseFloat(data.hits[i].recipe.yield);
+            
+            var hours = Math.floor( parseInt(data.hits[i].recipe.totalTime) / 60);          
+            var minutes = parseInt(data.hits[i].recipe.totalTime) % 60;
+            
+            var object = {
+                "image": data.hits[i].recipe.image,
+                "label": data.hits[i].recipe.label,
+                "url": data.hits[i].recipe.url,
+                "yield": data.hits[i].recipe.yield,
+                "dietLabels": data.hits[i].recipe.dietLabels,
+                "healthLabels": data.hits[i].recipe.healthLabels,
+                "ingredientLines": data.hits[i].recipe.ingredientLines,
+                "calories": Math.round(caloriesPer),
+                "totalTime": hours + " hours and " + minutes + " minutes"
+            };
+            arr.push(object);
+        }
+        res.json(arr);
+    }).catch(function(error) {
+	    if (error.response) {
+	      console.log(error.response.data);
+	      console.log(error.response.status);
+	      console.log(error.response.headers);
+	    } else if (error.request) {
+	      console.log(error.request);
+	    } else {
+	      console.log("Error", error.message);
+	    }
+	    console.log(error.config);
+	  });
 });
 
 // // Render 404 page for any unmatched routes
